ad92a471bb02d6ca30a6d90635b522eeed774ec2
Naymets91 <32165137+Naymets91@users.noreply.github.com>
2020-10-12 23:08:13 +0300

    +
    +

--- Viking/js/main.js
+++ Viking/js/main.js
@@ -1,11 +1,22 @@
 $(function () {
 
-    $('[data-fancybox]').fancybox({
-        youtube: {
-            controls: 0,
-            showinfo: 0
-        }
+    $('.heroes__slide-img').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: ' <button class="click-btn slick-prev"> <img src="images/arrof-left.png" alt="prev"></button>',
+        nextArrow: ' <button class="click-btn slick-next"> <img src="images/arrof-right.png" alt="next"></button>'
     });
 
+
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slide-img',
+        fade: true,
+        arrows: false, /* убрать кноки  */
+    });
 
 });
