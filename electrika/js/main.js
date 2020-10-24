d540dd410c64d42195d33b77680c7f26f6f3488d
Naymets91 <32165137+Naymets91@users.noreply.github.com>
2020-10-24 23:24:57 +0300

    3 страница слайдер

--- electrika/js/main.js
+++ electrika/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
+$(function () {
+
+    $('.galery__slider').slick({
+        dots: false,  /* убрать точки  */
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false, /* убрать кноки  */
+        prevArrow: ' <button class="click-btn slick-prev"> <img src="images/arrof-left.png" alt="prev"></button>',
+        nextArrow: ' <button class="click-btn slick-next"> <img src="images/arrof-right.png" alt="next"></button>',
+    });
+
 
 });
