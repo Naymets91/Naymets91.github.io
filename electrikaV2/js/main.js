72a14d5e5af08be3fd724c446da330e6e5324d91
Naymets91 <32165137+Naymets91@users.noreply.github.com>
2020-11-03 23:04:09 +0200

    2 v sait

---
+++ electrikaV2/js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('.sl').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        prevArrow: ' <button class="click-btn slick-prev"> <img src="images/arrof-left.png" alt="prev"></button>',
+        nextArrow: ' <button class="click-btn slick-next"> <img src="images/arrof-right.png" alt="next"></button>',
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    })
+
+
+
+});
